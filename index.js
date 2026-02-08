df862f9e0d22efd796f4372d21ecb5bc29e96d19
Adem Ozyavas <adem.ozyavas@atlas.edu.tr>
2026-02-08 23:32:52 +0300

    change welcome message - Dev A

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dev A');
+  res.send('Welcome to our website. ');
 });
 
 app.listen(3000);
