f262c6ef58eb523d8f6b81f351462f8dbd246b55
Adem Ozyavas <adem.ozyavas@atlas.edu.tr>
2026-02-08 23:41:45 +0300

    message change second time - Dev B

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello users this is  Dev B');
+  res.send('Changed and commited as second time - Dev B');
 });
 
 app.listen(3000);
